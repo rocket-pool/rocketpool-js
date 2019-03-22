eb5572624c8d15239dd1341f166a5298ad80d593
Jake Pospischil <moles1@gmail.com>
2019-03-22 11:44:19 +1000

    implemented register node method

--- src/rocketpool/node/node.ts
+++ src/rocketpool/node/node.ts
@@ -1,6 +1,10 @@
 // Imports
 import Web3 from 'web3';
+import Contract from 'web3/eth/contract';
+import { Tx } from 'web3/eth/types';
+import { TransactionReceipt } from 'web3/types';
 import Contracts from '../contracts/contracts';
+import { ConfirmationHandler, handleConfirmations } from '../../utils/transaction';
 
 
 /**
@@ -9,8 +13,25 @@ import Contracts from '../contracts/contracts';
 class Node {
 
 
+    // Contracts
+    private rocketNodeAPI: Promise<Contract>;
+
+
     // Constructor
-    public constructor(private web3: Web3, private contracts: Contracts) {}
+    public constructor(private web3: Web3, private contracts: Contracts) {
+        this.rocketNodeAPI = this.contracts.get('rocketNodeAPI');
+    }
+
+
+    // Register a node
+    public add(timezone: string, options?: Tx, onConfirmation?: ConfirmationHandler): Promise<TransactionReceipt> {
+        return this.rocketNodeAPI.then((rocketNodeAPI: Contract): Promise<TransactionReceipt> => {
+            return handleConfirmations(
+                rocketNodeAPI.methods.add(timezone).send(options),
+                onConfirmation
+            );
+        });
+    }
 
 
 }
