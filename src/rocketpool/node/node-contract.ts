76aebdcd8b3774e849b2e32907870b200a4fc565
Jake Pospischil <moles1@gmail.com>
2019-03-25 14:49:13 +1000

    implementing node module

--- src/rocketpool/node/node-contract.ts
+++ src/rocketpool/node/node-contract.ts
@@ -6,6 +6,26 @@ import { TransactionReceipt } from 'web3/types';
 import { ConfirmationHandler, handleConfirmations } from '../../utils/transaction';
 
 
+// Node details
+export interface NodeDetails {
+    owner: string;
+    rewardsAddress: string;
+    ethBalance: string;
+    rplBalance: string;
+    hasDepositReservation: boolean;
+}
+
+
+// Node deposit reservation details
+export interface NodeDepositReservation {
+    created: Date;
+    etherRequired: string;
+    rplRequired: string;
+    durationId: string;
+    depositInput: Buffer;
+}
+
+
 /**
  * RocketNodeContract instance wrapper
  */
@@ -19,6 +39,96 @@ class NodeContract {
     /**
      * Getters
      */
+
+
+    // Get all node details
+    public getDetails(): Promise<NodeDetails> {
+        return Promise.all([
+            this.getOwner(),
+            this.getRewardsAddress(),
+            this.getEthBalance(),
+            this.getRplBalance(),
+            this.getHasDepositReservation(),
+        ]).then(([owner, rewardsAddress, ethBalance, rplBalance, hasDepositReservation]: [string, string, string, string, boolean]): NodeDetails => {
+            return {owner, rewardsAddress, ethBalance, rplBalance, hasDepositReservation};
+        });
+    }
+
+
+    // Get all node deposit reservation details
+    public getDepositReservation(): Promise<NodeDepositReservation> {
+        return Promise.all([
+            this.getDepositReservationCreated(),
+            this.getDepositReservationEthRequired(),
+            this.getDepositReservationRplRequired(),
+            this.getDepositReservationDurationId(),
+            this.getDepositReservationDepositInput(),
+        ]).then(([created, etherRequired, rplRequired, durationId, depositInput]: [Date, string, string, string, Buffer]): NodeDepositReservation => {
+            return {created, etherRequired, rplRequired, durationId, depositInput};
+        });
+    }
+
+
+    // Get the node owner
+    public getOwner(): Promise<string> {
+        return this.contract.methods.getOwner().call();
+    }
+
+
+    // Get the node's rewards address
+    public getRewardsAddress(): Promise<string> {
+        return this.contract.methods.getRewardsAddress().call();
+    }
+
+
+    // Get the node's current ETH balance in wei
+    public getEthBalance(): Promise<string> {
+        return this.contract.methods.getBalanceETH().call();
+    }
+
+
+    // Get the node's current RPL balance in wei
+    public getRplBalance(): Promise<string> {
+        return this.contract.methods.getBalanceRPL().call();
+    }
+
+
+    // Check whether the node has an existing deposit reservation
+    public getHasDepositReservation(): Promise<boolean> {
+        return this.contract.methods.getHasDepositReservation().call();
+    }
+
+
+    // Get the deposit reservation created time
+    public getDepositReservationCreated(): Promise<Date> {
+        return this.contract.methods.getDepositReservedTime().call().then((created: string): Date => {
+            return new Date(parseInt(created) * 1000);
+        });
+    }
+
+
+    // Get the deposit reservation ETH requirement in wei
+    public getDepositReservationEthRequired(): Promise<string> {
+        return this.contract.methods.getDepositReserveEtherRequired().call();
+    }
+
+
+    // Get the deposit reservation RPL requirement in wei
+    public getDepositReservationRplRequired(): Promise<string> {
+        return this.contract.methods.getDepositReserveRPLRequired().call();
+    }
+
+
+    // Get the deposit reservation duration ID
+    public getDepositReservationDurationId(): Promise<string> {
+        return this.contract.methods.getDepositReserveDurationID().call();
+    }
+
+
+    // Get the deposit reservation DepositInput data
+    public getDepositReservationDepositInput(): Promise<Buffer> {
+        return this.contract.methods.getDepositReserveDepositInput().call();
+    }
 
 
     /**
--- src/test/node/node-tests.ts
+++ src/test/node/node-tests.ts
@@ -1,6 +1,7 @@
 // Imports
 import Web3 from 'web3';
 import RocketPool from '../../rocketpool/rocketpool';
+import NodeContract from '../../rocketpool/node/node-contract';
 import { registerNode, setNodeTimezone } from './node-scenarios';
 
 // Tests
@@ -13,8 +14,9 @@ export default function runNodeTests(web3: Web3, rp: RocketPool): void {
 
 
         // Node details
-        let nodeOwner: string;
-        let nodeContract: string;
+        let nodeOwnerAddress: string;
+        let nodeContractAddress: string;
+        let nodeContract: NodeContract;
 
 
         // Setup
@@ -23,6 +25,21 @@ export default function runNodeTests(web3: Web3, rp: RocketPool): void {
             // Get accounts
             let accounts: string[] = await web3.eth.getAccounts();
             owner = accounts[0];
+
+        });
+
+
+        // Node registration
+        describe('Registration', (): void => {
+
+            it('Can register a node', async () => {
+                [nodeOwnerAddress, nodeContractAddress] = await registerNode(web3, rp, 'foo/bar', {owner});
+                nodeContract = await rp.node.getContract(nodeContractAddress);
+            });
+
+            it('Can set the timezone location', async () => {
+                await setNodeTimezone(rp, 'bar/baz', {from: nodeOwnerAddress});
+            });
 
         });
 
@@ -38,20 +55,6 @@ export default function runNodeTests(web3: Web3, rp: RocketPool): void {
             it('Can get the current RPL requirement for an amount', async () => {
                 // :TODO: check values after node deposits made
                 let [rplRequired, rplRatio] = await rp.node.getRPLRequired(web3.utils.toWei('1', 'ether'), '3m');
-            });
-
-        });
-
-
-        // Node registration
-        describe('Registration', (): void => {
-
-            it('Can register a node', async () => {
-                [nodeOwner, nodeContract] = await registerNode(web3, rp, 'foo/bar', {owner});
-            });
-
-            it('Can set the timezone location', async () => {
-                await setNodeTimezone(rp, 'bar/baz', {from: nodeOwner});
             });
 
         });
