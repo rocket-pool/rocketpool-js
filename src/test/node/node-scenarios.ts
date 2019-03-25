8825bd997659b0544d772472b3f187f0bb8a75d7
Jake Pospischil <moles1@gmail.com>
2019-03-25 16:57:27 +1000

    minor test update

--- src/test/node/node-scenarios.ts
+++ src/test/node/node-scenarios.ts
@@ -10,29 +10,34 @@ export async function registerNode(web3: Web3, rp: RocketPool, {timezone, owner}
     const rocketPoolToken = await rp.contracts.get('rocketPoolToken');
 
     // Node owner and contract addresses
-    let nodeOwner: string = '';
-    let nodeContract: string = '';
+    let nodeOwnerAddress: string = '';
+    let nodeContractAddress: string = '';
 
     // Generate, seed and unlock node owner account
-    nodeOwner = await web3.eth.personal.newAccount('');
-    await web3.eth.sendTransaction({from: owner, to: nodeOwner, value: web3.utils.toWei('20', 'ether')});
-    await web3.eth.personal.unlockAccount(nodeOwner, '', 0);
+    nodeOwnerAddress = await web3.eth.personal.newAccount('');
+    await web3.eth.sendTransaction({from: owner, to: nodeOwnerAddress, value: web3.utils.toWei('20', 'ether')});
+    await web3.eth.personal.unlockAccount(nodeOwnerAddress, '', 0);
 
     // Add node
-    let result = await rp.node.add(timezone, {from: nodeOwner, gas: 8000000});
+    let result = await rp.node.add(timezone, {from: nodeOwnerAddress, gas: 8000000});
     assert.nestedProperty(result, 'events.NodeAdd.returnValues.contractAddress', 'Node was not registered successfully');
-    if (result.events !== undefined) nodeContract = result.events.NodeAdd.returnValues.contractAddress;
+    if (result.events !== undefined) nodeContractAddress = result.events.NodeAdd.returnValues.contractAddress;
 
     // Seed node contract
-    await web3.eth.sendTransaction({from: owner, to: nodeContract, value: web3.utils.toWei('5', 'ether')});
-    await rocketPoolToken.methods.mint(nodeContract, web3.utils.toWei('1000', 'ether')).send({from: owner, gas: 8000000});
+    await web3.eth.sendTransaction({from: owner, to: nodeContractAddress, value: web3.utils.toWei('5', 'ether')});
+    await rocketPoolToken.methods.mint(nodeContractAddress, web3.utils.toWei('1000', 'ether')).send({from: owner, gas: 8000000});
 
     // Check node contract address
-    let nodeContractTest = await rp.node.getContractAddress(nodeOwner);
-    assert.equal(nodeContractTest.toLowerCase(), nodeContract.toLowerCase(), 'Registered node contract address does not match');
+    let nodeContractAddressTest = await rp.node.getContractAddress(nodeOwnerAddress);
+    assert.equal(nodeContractAddressTest.toLowerCase(), nodeContractAddress.toLowerCase(), 'Registered node contract address does not match');
+
+    // Check node details
+    let nodeContract = await rp.node.getContract(nodeContractAddress);
+    let nodeOwnerAddressTest = await nodeContract.getOwner();
+    assert.equal(nodeOwnerAddressTest.toLowerCase(), nodeOwnerAddress.toLowerCase(), 'Node contract owner address does not match');
 
     // Return node owner and contract addresses
-    return [nodeOwner, nodeContract];
+    return [nodeOwnerAddress, nodeContractAddress];
 
 }
 
