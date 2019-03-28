690bac8826d3bde2e2d7e60113416e3799222059
Jake Pospischil <moles1@gmail.com>
2019-03-28 16:18:48 +1000

    implementing pool module tests

--- src/test/index.ts
+++ src/test/index.ts
@@ -7,6 +7,7 @@ import runContractsTests from './contracts/contracts-tests';
 import runDepositTests from './deposit/deposit-tests';
 import runGroupTests from './group/group-tests';
 import runNodeTests from './node/node-tests';
+import runPoolTests from './pool/pool-tests';
 import runSettingsTests from './settings/settings-tests';
 import runTokensTests from './tokens/tokens-tests';
 
@@ -21,5 +22,6 @@ runContractsTests(web3, rp);
 runDepositTests(web3, rp);
 runGroupTests(web3, rp);
 runNodeTests(web3, rp);
+runPoolTests(web3, rp);
 runSettingsTests(web3, rp);
 runTokensTests(web3, rp);
---
+++ src/test/pool/pool-tests.ts
@@ -0,0 +1,15 @@
+// Imports
+import { assert } from 'chai';
+import Web3 from 'web3';
+import RocketPool from '../../rocketpool/rocketpool';
+
+// Tests
+export default function runNodeTests(web3: Web3, rp: RocketPool): void {
+    describe('Pools', (): void => {
+
+
+        
+
+
+    });
+};
