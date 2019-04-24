56344f29068b381d9fe90e082670c8b02d802acf
Jake Pospischil <moles1@gmail.com>
2019-04-24 16:22:28 +1000

    reverted RPL and RPB class ERC20 changes

--- dist/rocketpool/tokens/rpl.js
+++ dist/rocketpool/tokens/rpl.js
@@ -1,52 +1,11 @@
-import { handleConfirmations } from '../../utils/transaction';
+import ERC20 from './ERC20';
 /**
  * Rocket Pool RPL token manager
  */
-class RPL {
+class RPL extends ERC20 {
     // Constructor
     constructor(web3, contracts) {
-        this.web3 = web3;
-        this.contracts = contracts;
-    }
-    // Contract accessor
-    get tokenContract() {
-        return this.contracts.get('rocketPoolToken');
-    }
-    /**
-     * Getters
-     */
-    // Get the token balance of an account
-    balanceOf(account) {
-        return this.tokenContract.then((tokenContract) => {
-            return tokenContract.methods.balanceOf(account).call();
-        });
-    }
-    // Get the allowance of a spender for an account
-    allowance(account, spender) {
-        return this.tokenContract.then((tokenContract) => {
-            return tokenContract.methods.allowance(account, spender).call();
-        });
-    }
-    /**
-     * Mutators - Public
-     */
-    // Transfer tokens to a recipient
-    transfer(to, amountWei, options, onConfirmation) {
-        return this.tokenContract.then((tokenContract) => {
-            return handleConfirmations(tokenContract.methods.transfer(to, amountWei).send(options), onConfirmation);
-        });
-    }
-    // Approve an allowance for a spender
-    approve(spender, amountWei, options, onConfirmation) {
-        return this.tokenContract.then((tokenContract) => {
-            return handleConfirmations(tokenContract.methods.approve(spender, amountWei).send(options), onConfirmation);
-        });
-    }
-    // Transfer tokens from an account to a recipient if approved
-    transferFrom(from, to, amountWei, options, onConfirmation) {
-        return this.tokenContract.then((tokenContract) => {
-            return handleConfirmations(tokenContract.methods.transferFrom(from, to, amountWei).send(options), onConfirmation);
-        });
+        super(web3, contracts, 'rocketPoolToken');
     }
 }
 // Exports
