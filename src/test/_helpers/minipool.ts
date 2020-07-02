16d2350ecdde7e93948e62aee8188871859d40e2
Jake Pospischil <moles1@gmail.com>
2020-07-02 14:35:42 +1000

    updated unit test helpers

---
+++ src/test/_helpers/minipool.ts
@@ -0,0 +1,32 @@
+// Imports
+import Web3 from 'web3';
+import { Contract, SendOptions } from 'web3-eth-contract/types';
+import RocketPool from '../../rocketpool/rocketpool';
+import MinipoolContract from '../../rocketpool/minipool/minipool-contract';
+import { getTxContractEvents } from '../_utils/contract';
+
+
+// Create a minipool
+export async function createMinipool(web3: Web3, rp: RocketPool, options: SendOptions): Promise<MinipoolContract | null> {
+
+    // Get contract addresses
+    const minipoolManagerAddress: string = await rp.contracts.rocketStorage.then((rocketStorage: Contract): Promise<string> => {
+        return rocketStorage.methods.getAddress(web3.utils.soliditySha3('contract.name', 'rocketMinipoolManager')).call();
+    });
+
+    // Make node deposit
+    let txReceipt = await rp.node.deposit(0, options);
+
+    // Get minipool created events
+    let minipoolCreatedEvents = getTxContractEvents(web3, txReceipt, minipoolManagerAddress, 'MinipoolCreated', [
+        {type: 'address', name: 'minipool', indexed: true},
+        {type: 'address', name: 'node', indexed: true},
+        {type: 'uint256', name: 'created'},
+    ]);
+
+    // Return minipool instance
+    if (!minipoolCreatedEvents.length) return null;
+    return rp.minipool.getMinipoolContract(minipoolCreatedEvents[0].minipool);
+
+}
+
---
+++ src/test/_utils/contract.ts
@@ -0,0 +1,24 @@
+// Imports
+import Web3 from 'web3';
+import { EventLog, TransactionReceipt } from 'web3-core';
+import { AbiInput } from 'web3-utils';
+
+
+// Get arbitrary contract events from a transaction result
+// txReceipt is the receipt returned from the transaction call
+// contractAddress is the address of the contract to retrieve events for
+// eventName is the name of the event to retrieve
+// eventParams is an array of objects with string 'type' and 'name' keys and an optional boolean 'indexed' key
+export function getTxContractEvents(web3: Web3, txReceipt: TransactionReceipt, contractAddress: string, eventName: string, eventParams: AbiInput[]) {
+    let events = [];
+    if (txReceipt.events) for (let p in txReceipt.events) events.push(txReceipt.events[p]);
+    return events
+        .filter((log: EventLog) => (log.address.toLowerCase() == contractAddress.toLowerCase()))
+        .filter((log: EventLog) => (log.raw!.topics[0] == web3.utils.soliditySha3(eventName + '(' + eventParams.map(param => param.type).join(',') + ')')))
+        .map((log: EventLog) => web3.eth.abi.decodeLog(eventParams.map(param => {
+            let decodeParam = Object.assign({}, param);
+            if (decodeParam.indexed && (decodeParam.type == 'string' || decodeParam.type == 'bytes')) decodeParam.type = 'bytes32'; // Issues decoding indexed string and bytes parameters
+            return decodeParam;
+        }), log.raw!.data, log.raw!.topics.slice(1)));
+}
+
