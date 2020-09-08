cbd120502621002950a1142cd4424b4d08615c03
Jake Pospischil <moles1@gmail.com>
2020-09-08 10:24:32 +1000

    build

--- dist/rocketpool/tokens/reth.js
+++ dist/rocketpool/tokens/reth.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _transaction = require('../../utils/transaction');
@@ -69,12 +71,26 @@ var RETH = function (_ERC) {
             });
         }
         // Get the total amount of ETH collateral available
+        // TODO: revert to getTotalCollateral call after new RP deployment
 
     }, {
         key: 'getTotalCollateral',
         value: function getTotalCollateral() {
-            return this.tokenContract.then(function (tokenContract) {
-                return tokenContract.methods.getTotalCollateral().call();
+            var _this3 = this;
+
+            //return this.tokenContract.then((tokenContract: Contract): Promise<string> => {
+            //    return tokenContract.methods.getTotalCollateral().call();
+            //});
+            return Promise.all([this.contracts.address('rocketETHToken').then(function (rethContractAddress) {
+                return _this3.web3.eth.getBalance(rethContractAddress);
+            }), this.contracts.get('rocketDepositPool').then(function (rocketDepositPool) {
+                return rocketDepositPool.methods.getExcessBalance().call();
+            })]).then(function (_ref) {
+                var _ref2 = _slicedToArray(_ref, 2),
+                    rethContractBalance = _ref2[0],
+                    depositPoolExcessBalance = _ref2[1];
+
+                return _this3.web3.utils.toBN(rethContractBalance).add(_this3.web3.utils.toBN(depositPoolExcessBalance)).toString(10);
             });
         }
         // Get the current ETH collateral rate
@@ -83,12 +99,12 @@ var RETH = function (_ERC) {
     }, {
         key: 'getCollateralRate',
         value: function getCollateralRate() {
-            var _this3 = this;
+            var _this4 = this;
 
             return this.tokenContract.then(function (tokenContract) {
                 return tokenContract.methods.getCollateralRate().call();
             }).then(function (value) {
-                return parseFloat(_this3.web3.utils.fromWei(value, 'ether'));
+                return parseFloat(_this4.web3.utils.fromWei(value, 'ether'));
             });
         }
         /**
